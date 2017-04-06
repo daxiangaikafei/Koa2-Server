a5250c440e310f27e5d6a404b37b20c6147b59b3
dol <allroad88888888@vip.qq.com>
2017-04-06 09:57:25 +0800

    up

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,10 +1,10 @@
 import * as Router from "koa-router";
 let routers_qbii:Router = require("./../modular/qbii/index");
-let routers_good:Router = require("./../modular/qgoodbii/index");
+let routers_good:Router = require("./../modular/good/index");
 
 let routes:Router[]= [
 	routers_qbii,
 	routers_good
 ];
 //module.exports = router
-export default routes;
+module.exports = routes;
