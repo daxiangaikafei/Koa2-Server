a06ad6e7627bc63c361b5c97765abdc8a2eba290
dol <allroad88888888@vip.qq.com>
2017-06-07 15:05:26 +0800

    s

--- app/modular/weixin/temp.ts
+++ app/modular/weixin/temp.ts
@@ -4,6 +4,13 @@ e&state=123#wechat_redirect
 
 
 
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=
+code&scope=snsapi_userinfo&state=STATE#wechat_redirect 
+
+】】
+
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8f9aecac8b1c5ac3&redirect_uri=http%3a%2f%2ftxdy.tunnel.qydev.com%2ftr1%2f&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
+
 http%3a%2f%2ftxdy.tunnel.qydev.com%2ftr1%2f
 
 154c78ce807aa090661d6497eeb94e87
